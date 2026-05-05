a3bf0456e64e4ee550af110d8ba0d9bb109740f4
Justin Plummer <justaplummer@gmail.com>
2026-05-05 16:38:25 -0500

    Player Movement
    Player can move around the game area and has movement clamped just within the boundary

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 
+@export var speed = 200
+var x_min = 225
+var x_max = 575
+var y_min = 25
+var y_max = 575
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
 
-
-func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
+	#position += velocity * delta
+	position.x = clamp(position.x, x_min, x_max)
+	position.y = clamp(position.y, y_min, y_max)
