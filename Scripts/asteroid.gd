42822a6c2c1e951d0e1b82d7656e41546f091fc9
Justin Plummer <justaplummer@gmail.com>
2026-05-08 13:34:35 -0500

    player detectable by asteroid
    player can take damage from the asteroid

--- Scripts/asteroid.gd
+++ Scripts/asteroid.gd
@@ -2,8 +2,17 @@ extends CharacterBody2D
 
 
 const SPEED = 150
-
+@export var damage_inflicted: int = 1
 
 func _physics_process(delta: float) -> void:
 	velocity.y = SPEED
 	move_and_slide()
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("hit ship")
+		var the_player: player = body
+		the_player.hit_asteroid(damage_inflicted)
+		print("player blow up animation")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,3 +1,4 @@
+class_name player
 extends CharacterBody2D
 
 @export var speed = 200
@@ -5,6 +6,8 @@ var x_min = 225
 var x_max = 575
 var y_min = 25
 var y_max = 575
+var max_health = 10
+var current_health = max_health 
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -16,3 +19,8 @@ func _physics_process(delta):
 	#position += velocity * delta
 	position.x = clamp(position.x, x_min, x_max)
 	position.y = clamp(position.y, y_min, y_max)
+	
+func hit_asteroid(health_subtract : int):
+	current_health -= health_subtract
+	if current_health < 0:
+		print("SHIP IS STRANDED!")
